4221140d1b6a16ea6999913a754c3dd8254dd672
poleski <pokiehl0>
2019-04-03 20:12:01 +0200

    Initial commit + angular core and cli update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { GeneralComponent } from './general/general.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { HistoryComponent } from './history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GeneralComponent,
+    SkillsComponent,
+    PortfolioComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule
